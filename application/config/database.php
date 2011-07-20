502966a862aac473bec89faf5841715701b7a263
Oliver Smith <chemicaloli@gmail.com>
2011-07-20 15:34:15 +0100

    Added default database parameters

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'ci-baseplate';
+$db['default']['password'] = 'ci-baseplate';
+$db['default']['database'] = 'ci-baseplate';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
